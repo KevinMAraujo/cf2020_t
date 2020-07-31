9a43096985e713bb2aea946702ac38c0f5488bd1
Kevin Araújo <11686566+KevinMAraujo@users.noreply.github.com>
2020-07-31 20:12:21 -0300

    Update server.py

--- server.py
+++ server.py
@@ -184,6 +184,6 @@ class ProductWithID(Resource):
         return ProductDAO().delete(id)
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.1', port=port)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.1', port=port)
+    #app.run()
